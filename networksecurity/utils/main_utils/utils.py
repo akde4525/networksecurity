efec1e369253d142f590a2aeaca46243c155e36b
Akash Dey <akde4525@gmail.com>
2025-11-07 15:19:11 +0530

    Data Validation

---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,32 @@
+import yaml
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+import sys
+import os
+import dill
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+    """
+    Reads a YAML file and returns its contents as a dictionary.
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            content = yaml.safe_load(yaml_file)
+        return content
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    """
+    Writes a dictionary to a YAML file.
+    """
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w') as yaml_file:
+            yaml.dump(content, yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)                
